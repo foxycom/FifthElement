e887f9d7a1b81da77cfb04b5d0f39f3b8e82a8f0
Vsevolod Tymofyeyev <timvseffolod@gmail.com>
2022-03-31 16:31:27 +0200

    Update project structure

--- TheElement/app/src/main/java/fifthelement/theelement/persistence/hsqldb/PersistenceException.java
+++
@@ -1,8 +0,0 @@
-package fifthelement.theelement.persistence.hsqldb;
-
-public class PersistenceException extends RuntimeException {
-    public PersistenceException(final Exception cause) {
-        super(cause);
-    }
-}
-
